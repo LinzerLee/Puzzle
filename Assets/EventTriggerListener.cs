308843f49f382bf2dd0e17f450ab9747ed3678c4
Linzer Lee <Linzer_Lee@hotmail.com>
2016-06-16 12:32:21 +0800

    Initial contents of Puzzle

---
+++ Assets/EventTriggerListener.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class EventTriggerListener : UnityEngine.EventSystems.EventTrigger
+{
+    public delegate void VoidDelegate(GameObject go);
+    public delegate void BoolDelegate(GameObject go, bool state);
+    public delegate void FloatDelegate(GameObject go, float delta);
+    public delegate void VectorDelegate(GameObject go, Vector2 delta);
+    public delegate void ObjectDelegate(GameObject go, GameObject obj);
+    public delegate void KeyCodeDelegate(GameObject go, KeyCode key);
+    public VoidDelegate onClick;
+    public VoidDelegate onDown;
+    public VoidDelegate onEnter;
+    public VoidDelegate onExit;
+    public VoidDelegate onUp;
+    public VoidDelegate onSelect;
+    public VoidDelegate onUpdateSelect;
+    public VectorDelegate onBeginDrag;
+    public VectorDelegate onDrag;
+    public VectorDelegate onMove;
+
+    static public EventTriggerListener Get(GameObject go)
+    {
+        EventTriggerListener listener = go.GetComponent<EventTriggerListener>();
+        if (listener == null)
+            listener = go.AddComponent<EventTriggerListener>();
+
+        return listener;
+    }
+    static public EventTriggerListener Get(Transform transform)
+    {
+        EventTriggerListener listener = transform.GetComponent<EventTriggerListener>();
+        if (listener == null)
+            listener = transform.gameObject.AddComponent<EventTriggerListener>();
+
+        return listener;
+    }
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if (onClick != null)
+            onClick(gameObject);
+    }
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        if (onDown != null)
+            onDown(gameObject);
+    }
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        if (onEnter != null)
+            onEnter(gameObject);
+    }
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        if (onExit != null)
+            onExit(gameObject);
+    }
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        if (onUp != null)
+            onUp(gameObject);
+    }
+    public override void OnSelect(BaseEventData eventData)
+    {
+        if (onSelect != null)
+            onSelect(gameObject);
+    }
+    public override void OnUpdateSelected(BaseEventData eventData)
+    {
+        if (onUpdateSelect != null)
+            onUpdateSelect(gameObject);
+    }
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        if (onBeginDrag != null)
+            onBeginDrag(gameObject, eventData.delta);
+    }
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        if (onDrag != null)
+            onDrag(gameObject, eventData.delta);
+    }
+    public override void OnMove(AxisEventData eventData)
+    {
+        if (onMove != null)
+            onMove(gameObject, eventData.moveVector);
+    }
+}
---
+++ Assets/cube.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+    public float speed = 0.1F;
+
+    private Transform[] cubes;
+    // Use this for initialization
+    void Start () {
+        cubes = transform.parent.GetComponentInChildren<Transform>();
+        EventTriggerListener.Get(gameObject).onDown += (go) =>
+        {
+            transform.localScale = new Vector3(1.2f, 1.2f, 1f);
+            
+            
+        };
+
+        EventTriggerListener.Get(gameObject).onUp += (go) =>
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        };
+
+        EventTriggerListener.Get(gameObject).onBeginDrag += (go, delta) => 
+        {
+            transform.Translate(delta.x * speed, delta.y * speed, 0);
+        };
+
+        EventTriggerListener.Get(gameObject).onDrag += (go, delta) =>
+        {
+            transform.Translate(delta.x * speed, delta.y * speed, 0);
+        };
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+            Debug.Log("拖动");
+        }
+    }
+}
